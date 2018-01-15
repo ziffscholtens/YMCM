41ca639731af323a7e9d034c0518c15bcf672811
tho12 <35299032+tho12@users.noreply.github.com>
2018-01-15 11:28:47 +0100

    Update House.java

--- House.java
+++ House.java
@@ -1,31 +1,46 @@
-package Heuristics;
+package amstelhaege;
 
 public class House {
 	/*
 	 * Coordinates kan Linksboven rechtsonder of Alle Hoeken
 	 */
-	int value;
-	int type;
-	Coordinates coordinates;
+	double value;
+	String type; //0 for Mansion, 1 for Bungalow, 2 for Family House
+	int x;
+	int y;
+	int len1;
+	int len2;
+	//Coordinates coordinates;
 	
 	House(){
 	}
 	
-	House(int value, int type, Coordinates coordinates){
-		this.value = value;
+	House(int x, int y, int len1, int len2, String type){
+		this.x = x;
+		this.y = y;
+		this.len1 = len1;
+		this.len2 = len2;
 		this.type = type;
-		this.coordinates = coordinates;
+//		this.coordinates = coordinates;
 	}
 	
-	int getValue(){
-		return value;
+	void setValue(double worth) {
+		value = worth;
 	}
 	
-	int getType(){
-		return type;
+	int clearance() {
+		int clearance = 0;
+		if(type.equals("mansion")) {
+			clearance = 60;
+		} else if (type.equals("bungalow")) {
+			clearance = 30;
+		} else if (type.equals("famHouse")) {
+			clearance = 20;
+		}
+		return clearance;
 	}
 	
-	Coordinates getCoordinates(){
-		return coordinates;
-	}
+//	Coordinates getCoordinates(){
+//		return coordinates;
+//	}
 }
